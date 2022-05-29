1b9d895229415ef981323f448710cdc192388680
MyLadySnezhka <101964822+MyLadySnezhka@users.noreply.github.com>
2022-05-29 13:42:06 +0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+let chel1 = 38;
+let chel2 = 50;
+let chel3 = 24;
+
+if(chel1 < 18) {
+    console.log('go to school');    
+}   else if(chel2 > 60) {
+        console.log('pension');
+}   else if(chel3 > 20) {
+        console.log('university');
+}
+
+console.log('CONGRAT!');
