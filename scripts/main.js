765af2cebf291adcedf5461f7482bb0a3277a86d
MyLadySnezhka <mylady.snezhka@gmail.com>
2022-06-02 11:58:09 +0300

    add mess

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,36 @@
 
 const foodlink = document.querySelector('select[name="food"]');
 const refrlink = document.querySelector('.content-block2');
+const messlink = document.querySelector('.content-block3');
+
+let count = 0;
+let fullrefr = 0;
 
 foodlink.addEventListener('click', (ev) => {
 
     let fooditem = foodlink.value;
 
-    if (fooditem === 'jirafe') {
-    refrlink.innerHTML = 'НЕТ МЕСТА! Вынь слона!!!'
+    if(count > 5) {
+        messlink.innerHTML = "Холодильник переполнен!"
+        return;
     }
 
-console.log(fooditem);
+    if(fooditem === 'elefant') {
+        fullrefr = 1;
+    }
+
+    if(fooditem === 'jirafe' && fullrefr == 1) {
+        messlink.innerHTML = 'НЕТ МЕСТА! Вынь слона!!!'
+        return;
+    }
+
+    else {
+        refrlink.insertAdjacentHTML = ('afterend', fooditem);
+        refrlink.innerHTML = fooditem;
+        count = count + 1;
+    }
+
+    console.log(count);
 
 })
 
