17d0a39a33b69912352d28c3e56a19db844a0326
MyLadySnezhka <101964822+MyLadySnezhka@users.noreply.github.com>
2022-06-02 03:26:03 +0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,19 @@
-let chel1 = 38;
-let chel2 = 50;
-let chel3 = 24;
+const fooditem = document.querySelector('[select="food"]');
 
-if(chel1 < 18) {
-    console.log('go to school');    
-}   else if(chel2 > 60) {
-        console.log('pension');
-}   else if(chel3 > 20) {
-        console.log('university');
+const run = () => {
+    const foodid = fooditem.value;
+    console.log(foodid);
+
+
+    if(foodid === 'жираф') {
+    contentblock3.innerHTML = 'Вынь слона!!!';
 }
 
-console.log('CONGRAT!');
+}
+
+run();
+fooditem.addEventListener('change', run);
+
+
+
+
